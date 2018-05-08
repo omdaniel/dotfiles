dba863e34ca9e19a85dccb08d120e44c41d05b35
Oliver M. Daniel <olivermdaniel@gmail.com>
2018-05-08 17:31:48 +0000

    fixed quotes from html to ascii

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-ln -sv “~/.dotfiles/runcom/.bash_profile” ~
-ln -sv “~/.dotfiles/runcom/.inputrc” ~
-ln -sv “~/.dotfiles/git/.gitconfig” ~
+ln -sv "~/.dotfiles/runcom/bash_profile" ~/.bash_profile
+ln -sv "~/.dotfiles/runcom/.inputrc" ~/.inputrc
+ln -sv "~/.dotfiles/git/.gitconfig" ~/.gitconfig
