7bf95da68839022e220ddfdccf2fe2c02200fc47
Oliver M. Daniel <olivermdaniel@gmail.com>
2018-05-08 18:29:07 +0000

    added alias.sh and functions.sh

---
+++ system/alias.sh
@@ -0,0 +1,5 @@
+alias l="ls -la"       # List in long format, include dotfiles
+alias lf="ls -ld */"   # List in long format, only directories
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
