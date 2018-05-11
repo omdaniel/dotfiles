ad50a96ae36a16ed3c761dc4442f2cb38f991a69
Ubuntu <ubuntu@ip-172-31-38-30.ec2.internal>
2018-05-11 13:10:18 +0000

    reset to remove vim w/ vim-plug, restored current install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
-ln -sv "~/.dotfiles/runcom/bash_profile" ~/.bash_profile
-ln -sv "~/.dotfiles/runcom/.inputrc" ~/.inputrc
-ln -sv "~/.dotfiles/git/.gitconfig" ~/.gitconfig
+ln -sv "$HOME/dotfiles/runcom/bash_profile" ~/.bash_profile
+ln -sv "$HOME/dotfiles/runcom/inputrc" ~/.inputrc
+ln -sv "$HOME/dotfiles/git/gitconfig" ~/.gitconfig
+ln -sv "$HOME/dotfiles/vim/vimrc" ~/.vimrc
+ln -sv "$HOME/dotfiles/tmux.conf" ~/.tmux.conf
+
+[ ! -d "$HOME/.vim" ] && ln -sv "$HOME/dotfiles/vim"  ~/.vim
