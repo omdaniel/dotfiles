5a759fb8f3ceb24e453f612b006422d84dfaa3c8
Oliver M. Daniel <olivermdaniel@gmail.com>
2018-05-11 14:15:25 +0000

    Added vim packages as submodules

---
+++ vimsub.sh
@@ -0,0 +1,76 @@
+git submodule add https://github.com/MarcWeber/vim-addon-local-vimrc vim/pack/default/start/vim-addon-local-vimrc
+git add .gitmodules vim/pack/default/start/vim-addon-local-vimrc
+git submodule add https://github.com/easymotion/vim-easymotion vim/pack/default/start/vim-easymotion
+git add .gitmodules vim/pack/default/start/vim-easymotion
+git submodule add https://github.com/vim-airline/vim-airline vim/pack/default/start/vim-airline
+git add .gitmodules vim/pack/default/start/vim-airline
+git submodule add https://github.com/vim-airline/vim-airline-themes vim/pack/default/start/vim-airline-themes
+git add .gitmodules vim/pack/default/start/vim-airline-themes
+git submodule add https://github.com/scrooloose/nerdtree vim/pack/default/start/nerdtree
+git add .gitmodules vim/pack/default/start/nerdtree
+git submodule add https://github.com/Xuyuanp/nerdtree-git-plugin vim/pack/default/start/nerdtree-git-plugin
+git add .gitmodules vim/pack/default/start/nerdtree-git-plugin
+git submodule add https://github.com/tpope/vim-fugitive vim/pack/default/start/vim-fugitive
+git add .gitmodules vim/pack/default/start/vim-fugitive
+git submodule add https://github.com/gregsexton/gitv vim/pack/default/start/gitv
+git add .gitmodules vim/pack/default/start/gitv
+git submodule add https://github.com/kablamo/vim-git-log vim/pack/default/start/vim-git-log
+git add .gitmodules vim/pack/default/start/vim-git-log
+git submodule add https://github.com/junegunn/fzf.vim vim/pack/default/start/fzf.vim
+git add .gitmodules vim/pack/default/start/fzf.vim
+git submodule add https://github.com/vim-scripts/YankRing.vim vim/pack/default/start/YankRing.vim
+git add .gitmodules vim/pack/default/start/YankRing.vim
+git submodule add https://github.com/sjl/gundo.vim vim/pack/default/start/gundo.vim
+git add .gitmodules vim/pack/default/start/gundo.vim
+git submodule add https://github.com/tpope/vim-abolish vim/pack/default/start/vim-abolish
+git add .gitmodules vim/pack/default/start/vim-abolish
+git submodule add https://github.com/mattn/webapi-vim vim/pack/default/start/webapi-vim
+git add .gitmodules vim/pack/default/start/webapi-vim
+git submodule add https://github.com/mattn/gist-vim vim/pack/default/start/gist-vim
+git add .gitmodules vim/pack/default/start/gist-vim
+git submodule add https://github.com/terryma/vim-multiple-cursors vim/pack/default/start/vim-multiple-cursors
+git add .gitmodules vim/pack/default/start/vim-multiple-cursors
+git submodule add https://github.com/skywind3000/asyncrun.vim vim/pack/default/start/asyncrun.vim
+git add .gitmodules vim/pack/default/start/asyncrun.vim
+git submodule add https://github.com/junegunn/vim-easy-align vim/pack/default/start/vim-easy-align
+git add .gitmodules vim/pack/default/start/vim-easy-align
+git submodule add https://github.com/Yggdroot/indentLine vim/pack/default/start/indentLine
+git add .gitmodules vim/pack/default/start/indentLine
+git submodule add https://github.com/Konfekt/FastFold vim/pack/default/start/FastFold
+git add .gitmodules vim/pack/default/start/FastFold
+git submodule add https://github.com/jiangmiao/auto-pairs vim/pack/default/start/auto-pairs
+git add .gitmodules vim/pack/default/start/auto-pairs
+git submodule add https://github.com/tpope/vim-commentary vim/pack/default/start/vim-commentary
+git add .gitmodules vim/pack/default/start/vim-commentary
+git submodule add https://github.com/tmhedberg/matchit vim/pack/default/start/matchit
+git add .gitmodules vim/pack/default/start/matchit
+git submodule add https://github.com/tpope/vim-surround vim/pack/default/start/vim-surround
+git add .gitmodules vim/pack/default/start/vim-surround
+git submodule add https://github.com/w0rp/ale vim/pack/default/start/ale
+git add .gitmodules vim/pack/default/start/ale
+git submodule add https://github.com/maralla/completor.vim vim/pack/default/start/completor.vim
+git add .gitmodules vim/pack/default/start/completor.vim
+git submodule add https://github.com/SirVer/ultisnips vim/pack/default/start/ultisnips
+git add .gitmodules vim/pack/default/start/ultisnips
+git submodule add https://github.com/honza/vim-snippets vim/pack/default/start/vim-snippets
+git add .gitmodules vim/pack/default/start/vim-snippets
+git submodule add https://github.com/ervandew/supertab vim/pack/default/start/supertab
+git add .gitmodules vim/pack/default/start/supertab
+git submodule add https://github.com/joonty/vdebug vim/pack/default/start/vdebug
+git add .gitmodules vim/pack/default/start/vdebug
+git submodule add https://github.com/jaxbot/semantic-highlight.vim vim/pack/default/start/semantic-highlight.vim
+git add .gitmodules vim/pack/default/start/semantic-highlight.vim
+git submodule add https://github.com/majutsushi/tagbar vim/pack/default/start/tagbar
+git add .gitmodules vim/pack/default/start/tagbar
+git submodule add https://github.com/metakirby5/codi.vim vim/pack/default/start/codi.vim
+git add .gitmodules vim/pack/default/start/codi.vim
+git submodule add https://github.com/ryanoasis/vim-devicons vim/pack/default/start/vim-devicons
+git add .gitmodules vim/pack/default/start/vim-devicons
+git submodule add https://github.com/Vimjas/vim-python-pep8-indent vim/pack/default/start/vim-python-pep8-indent
+git add .gitmodules vim/pack/default/start/vim-python-pep8-indent
+git submodule add https://github.com/davidhalter/jedi-vim vim/pack/default/start/jedi-vim
+git add .gitmodules vim/pack/default/start/jedi-vim
+git submodule add https://github.com/tmhedberg/SimpylFold vim/pack/default/start/SimpylFold
+git add .gitmodules vim/pack/default/start/SimpylFold
+git submodule add https://github.com/python-rope/ropevim vim/pack/default/start/ropevim
+git add .gitmodules vim/pack/default/start/ropevim
